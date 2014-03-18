ddc4eac5ca2d63bb1a3b0376a67774a981a7c7ec
Fisher Adelakin <temidayoadelakin@gmail.com>
2014-03-18 15:15:18 -0400

    problem 3

---
+++ src/Problem3.java
@@ -0,0 +1,31 @@
+public class Problem3 {
+	
+	public static boolean isPrime(int n) {
+        if (n == 2) 
+        	return true;
+        if (n % 2 == 0 || n == 1) 
+        	return false;
+        long limit = (long)Math.sqrt(n);
+        for (int i = 3; i <= limit; i += 2)
+            if (n % i == 0) 
+            	return false;
+        return true;
+	}
+ 
+	public static void main(String args[]) {
+        long n = Long.parseLong("600851475143");
+        int max = 0;
+        while (n != 1) {
+            int i = 2;
+            while (i <= n) {
+                if (isPrime(i) && n % i == 0) {
+                    if (i > max) max = i;
+                    n /= i;
+                    break;
+                }
+                ++i;
+            }
+        }
+        System.out.println(max);
+}
+}
