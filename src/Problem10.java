cfe8591e0876be959f10d17376616e877b6c9d81
Fisher Adelakin <temidayoadelakin@gmail.com>
2014-03-26 18:50:00 -0400

    problem 10

--- src/Problem10.java
+++ src/Problem10.java
@@ -1,4 +1,22 @@
 
 public class Problem10 {
 
+	public static void main(String[] args) {
+		long sum = 0;
+		int n = 2000000;
+		for(int i = 2; i < n; i++) {
+			if(isPrime(i)) {
+				sum += i;
+			}
+		}
+		System.out.println(sum);
+	}
+	
+	public static boolean isPrime(long n) {
+		boolean isPrime = true;
+		for(int i = 2; i <= Math.round(Math.sqrt(n)) && isPrime; i++) {
+			isPrime = !(n % i == 0);
+		}
+		return isPrime;
+	}
 }
